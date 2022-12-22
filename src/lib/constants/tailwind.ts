ad750f77b01b843108723beef1fde6d585e011e4
aurelticot <aurelticot.dev@gmail.com>
2022-12-22 20:29:58 +1100

    update tailwind config

---
+++ src/lib/constants/tailwind.ts
@@ -0,0 +1,3 @@
+export const screenSizes = {
+  sm: "696px",
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,38 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
+      "primary": "#ffffff",
+      "primary-inverse": "#000000",
+      "background": "#0D0A27",
+      "background-button": "rgba(255, 255, 255, 0.15)",
+      "background-button-hover": "rgba(255, 255, 255, 0.3);",
       "white": "#ffffff",
-      "black": "#0000",
+      "black": "#000000",
       "error": "#CE4B46",
       "green": "#13ce66",
       "gray-dark": "rgba(255, 255, 255, 0.15);",
       "gray-light": "#9D9D9D",
-      "background": "#0D0A27",
       "verida-blue": "#37D5C7",
       "verida-purple": "#5354D1",
-      "transparent": `rgba(17, 17, 17, 0.2)`
+      "transparent": `rgba(17, 17, 17, 0.2)`,
     },
     fontFamily: {
-      sora: ['Sora', 'sans-serif']
+      sans: ["Sora", "sans-serif"],
     },
     extend: {
+      screens: {
+        sm: "696px",
+      },
+      spacing: {
+        4.5: "1.125rem",
+      },
       backdropBlur: {
-        xs: '3px',
-      }
+        xs: "3px",
+      },
+      backgroundImage: {
+        "top-gradient": "url('./images/top-bg-gradient.png')",
+        "bottom-gradient": "url('./images/bottom-bg-gradient.png')",
+      },
     },
   },
   plugins: [],
