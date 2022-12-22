f85f6076608663c1f7e4faedb42d73732212ec81
aurelticot <aurelticot.dev@gmail.com>
2022-12-22 20:42:41 +1100

    revert components to focus on atoms and molecules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
 import React from "react";
-import { Header } from "components/organisms";
-import { ProfileView } from "components/pages";
-import { screenSize } from "lib/constants";
 
 export const App = () => {
-  return (
-    <div className="h-screen">
-      <Header />
-      <main
-        className={`mx-auto px-4 py-3.5 md:flex md:w-[${screenSize.sm}] md:flex-col md:items-center md:justify-center`}
-      >
-        <ProfileView />
-      </main>
-    </div>
-  );
+  return <div className="h-screen"></div>;
 };
