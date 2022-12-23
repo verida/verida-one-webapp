1857ce715de3e09b563c951a731889216b0b15ff
aurelticot <aurelticot.dev@gmail.com>
2022-12-23 11:26:15 +1100

    update weboack config to ignore some warnings

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,8 @@
 const webpack = require("webpack");
+
 module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
+
   Object.assign(fallback, {
     http: require.resolve("stream-http"),
     https: require.resolve("https-browserify"),
@@ -11,8 +13,14 @@ module.exports = function override(config) {
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       process: "process/browser",
-      Buffer: [require.resolve("buffer/"), "Buffer"]
+      Buffer: [require.resolve("buffer/"), "Buffer"],
     }),
   ]);
+
+  // Ignore some warnings
+  config.ignoreWarnings = [
+    /Failed to parse source map/, // until fixed by cra https://github.com/facebook/create-react-app/pull/11752
+  ];
+
   return config;
 };
