e2dfa70c816f181f0fb606366f00070edaf118e3
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-01-05 22:26:28 +0100

    feat:added lang traslation for collection list page #7

--- src/components/pages/CollectibleListView.tsx
+++ src/components/pages/CollectibleListView.tsx
@@ -3,9 +3,18 @@ import { CollectiblesList } from "components/organisms";
 import { Collectible } from "lib/types";
 import { getCollectibles } from "lib/utils";
 import React, { useEffect, useState } from "react";
+import { useIntl } from "react-intl";
 
 export const CollectibleListView: React.FunctionComponent = () => {
+  const i18n = useIntl();
   const [collectibles, setCollectibles] = useState<Collectible[]>([]);
+
+  const sectionTitle = i18n.formatMessage({
+    id: "CollectiblesListPage.sectionTitle",
+    description:
+      "Title of the 'Collectibles' section in the 'Collectible list view' page",
+    defaultMessage: "Collectibles",
+  });
 
   useEffect(() => {
     const getData = async () => {
@@ -15,7 +24,7 @@ export const CollectibleListView: React.FunctionComponent = () => {
   }, []);
 
   return (
-    <AssetPageWrapper title="Collectibles" badgeCount={collectibles.length}>
+    <AssetPageWrapper title={sectionTitle} badgeCount={collectibles.length}>
       <CollectiblesList collectibles={collectibles} />
     </AssetPageWrapper>
   );
