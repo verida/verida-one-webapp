ba78940b88354d431e64c7ebe2a698a6ee205cc8
aurelticot <aurelticot.dev@gmail.com>
2023-01-06 14:12:24 +1100

    allow choosing a mock profile

--- src/lib/utils/profileUtils.ts
+++ src/lib/utils/profileUtils.ts
@@ -5,40 +5,51 @@ import {
   SocialMediaLink,
   WalletAddress,
 } from "lib/types";
-import {
-  profileInfo,
-  WalletAddresses,
-  collectibles,
-  featuredCollectibles,
-  featuredLinks,
-  links,
-  socialMediaLinks,
-} from "mock/data";
+import { getIdentityProfile } from "mock/data";
 
-export const getProfileInfo = async (): Promise<ProfileInfo> => {
-  return Promise.resolve(profileInfo);
+export const getProfileInfo = async (
+  identity?: string
+): Promise<ProfileInfo> => {
+  const { profileInfo } = await getIdentityProfile(identity);
+  return profileInfo;
 };
 
-export const getWalletAddresses = async (): Promise<WalletAddress[]> => {
-  return Promise.resolve(WalletAddresses);
+export const getWalletAddresses = async (
+  identity?: string
+): Promise<WalletAddress[]> => {
+  const { walletAddresses } = await getIdentityProfile(identity);
+  return walletAddresses;
 };
 
-export const getCollectibles = async (): Promise<Collectible[]> => {
-  return Promise.resolve(collectibles);
+export const getCollectibles = async (
+  identity?: string
+): Promise<Collectible[]> => {
+  const { collectibles } = await getIdentityProfile(identity);
+  return collectibles;
 };
 
-export const getFeaturedCollectibles = async (): Promise<Collectible[]> => {
-  return Promise.resolve(featuredCollectibles);
+export const getFeaturedCollectibles = async (
+  identity?: string
+): Promise<Collectible[]> => {
+  const { featuredCollectibles } = await getIdentityProfile(identity);
+  return featuredCollectibles;
 };
 
-export const getFeaturedLinks = async (): Promise<CustomLink[]> => {
-  return Promise.resolve(featuredLinks);
+export const getFeaturedLinks = async (
+  identity?: string
+): Promise<CustomLink[]> => {
+  const { featuredLinks } = await getIdentityProfile(identity);
+  return featuredLinks;
 };
 
-export const getLinks = async (): Promise<CustomLink[]> => {
-  return Promise.resolve(links);
+export const getLinks = async (identity?: string): Promise<CustomLink[]> => {
+  const { links } = await getIdentityProfile(identity);
+  return links;
 };
 
-export const getSocialMediaLinks = async (): Promise<SocialMediaLink[]> => {
-  return Promise.resolve(socialMediaLinks);
+export const getSocialMediaLinks = async (
+  identity?: string
+): Promise<SocialMediaLink[]> => {
+  const { socialMediaLinks } = await getIdentityProfile(identity);
+  return socialMediaLinks;
 };
--- src/mock/data.ts
+++ src/mock/data.ts
@@ -1,4 +1,18 @@
 // Temporary data for development purpose
+import { Profile } from "lib/types";
+import { profile as profile6529 } from "./data-6529";
+import { profile as profileJohnDoe } from "./data-johndoe";
+import { profile as profileRyan } from "./data-ryan";
 
-export * from "./data-6529";
-// export * from './data-johndoe'
+export const getIdentityProfile = async (
+  identityId?: string
+): Promise<Profile> => {
+  switch (identityId) {
+    case "6529":
+      return Promise.resolve(profile6529);
+    case "johndoe":
+      return Promise.resolve(profileJohnDoe);
+    default:
+      return Promise.resolve(profileRyan);
+  }
+};
