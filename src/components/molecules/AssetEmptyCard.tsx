19ed0daa35f8553c14742cc38f39c12e2b53401d
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-01-12 15:12:26 +0100

    feat: refactor assetemptycard component

--- src/components/molecules/AssetEmptyCard.tsx
+++ src/components/molecules/AssetEmptyCard.tsx
@@ -1,39 +1,31 @@
 import { Button } from "components/atoms";
 import React from "react";
-import { useIntl } from "react-intl";
 import { Link } from "react-router-dom";
 
 type AssetEmptyCardProps = {
   title: string;
   message: string;
-  identity?: string;
+  redirectPath: string;
+  buttonLabel: string;
 } & React.ComponentPropsWithRef<"div">;
 
 /** Card to display empty assets list or details in assets pages*/
 export const AssetEmptyCard: React.FunctionComponent<AssetEmptyCardProps> = ({
   message,
   title,
-  identity,
+  buttonLabel,
+  redirectPath,
   ...otherProps
 }) => {
-  const i18n = useIntl();
-  const goToProfilePageLink = identity ? `/${identity}` : `/`;
-
-  const goToProfilePageLinkLabel = i18n.formatMessage({
-    id: "AssetEmptyCard.goToProfilePageLinkLabel",
-    description: "Go to 'profile' page link label",
-    defaultMessage: "Go to profile",
-  });
-
   return (
     <div {...otherProps}>
       <div className="flex flex-col items-center justify-center rounded-xl bg-gray-dark p-4">
         <span className="mb-2 text-lg font-semibold">{title}</span>
         <p className="mb-4 text-sm">{message}</p>
         <div className="w-full">
-          <Link to={goToProfilePageLink}>
+          <Link to={redirectPath}>
             <Button variant="contained" size="large">
-              {goToProfilePageLinkLabel}
+              {buttonLabel}
             </Button>
           </Link>
         </div>
