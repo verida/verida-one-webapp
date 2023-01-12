08d021e2bd1f0973ca5425a13d38506616af6615
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-01-12 15:16:10 +0100

    aligned and clean up unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,16 @@ import { AppLayout } from "components/templates";
 /**
  * Routes
  *
- * /                                                       -> Home
- * /:identity                                              -> Profile
- * /:identity/collectibles                                 -> CollectibleList
- * /:identity/collectibles/:chain/:contractAddress/:tokenId-> CollectibleDetails
- * /:identity/badges                                       -> BadgeList
- * /:identity/badges/:chain/:contractAddress/:tokenId                           -> BadgeDetails
+ * /                                                          -> Home
+ * /:identity                                                 -> Profile
+ * /:identity/collectibles                                    -> CollectibleList
+ * /:identity/collectibles/:chain/:contractAddress/:tokenId   -> CollectibleDetails
+ * /:identity/badges                                          -> BadgeList
+ * /:identity/badges/:chain/:contractAddress/:tokenId         -> BadgeDetails
  *
- * TODO: Strenghten with a list of routes as constant to be used throughout the app
+ * TODO: Strengthen with a list of routes as constant to be used throughout the app
  */
-//http://localhost:3000/ryan/badges/ethereum/0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270/78000314
+
 export const App: React.FunctionComponent = () => {
   return (
     <Router>
