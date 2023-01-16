bcbb46c1ac7314756c2eefec9c2912e6753256ff
aurelticot <aurelticot.dev@gmail.com>
2023-01-16 16:48:36 +1100

    conditionally display the app gradient background

--- src/components/templates/AppLayout.tsx
+++ src/components/templates/AppLayout.tsx
@@ -1,14 +1,21 @@
 import { Footer, Header, HeaderOffset } from "components/organisms";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useMatch } from "react-router-dom";
 
 export const AppLayout: React.FunctionComponent = () => {
+  // Identitfy when the current page is the profile
+  const profileRouteMatch = useMatch("/:identity");
+
   return (
     <div className="relative flex h-full w-full flex-col">
       <div className="fixed top-0 left-0 right-0 z-50 backdrop-blur-[6px]">
         <Header />
       </div>
-      <div className="bg-app flex min-h-screen flex-col">
+      <div
+        className={`flex min-h-screen flex-col ${
+          profileRouteMatch ? "bg-app" : ""
+        }`}
+      >
         <HeaderOffset />
         <main className="mx-auto flex w-full max-w-screen-sm flex-grow flex-col px-4 pt-4">
           <Outlet />
