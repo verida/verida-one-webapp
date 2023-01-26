1aa3905a273ce0db6482060715692371fe917826
aurelticot <aurelticot.dev@gmail.com>
2023-01-26 17:07:13 +1100

    create loading skeleton for identity info section

--- src/components/organisms/IdentityInfoSection.tsx
+++ src/components/organisms/IdentityInfoSection.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { IconButton, Icon, Avatar } from "components/atoms";
+import { IconButton, Icon, Avatar, SkeletonBase } from "components/atoms";
 import { IdentityInfo } from "lib/types";
 
 type IdentityInfoSectionProps = {
-  identityInfo: IdentityInfo;
+  identityInfo?: IdentityInfo;
 };
 
 /** Section for the Profile page rendering the main information */
@@ -12,33 +12,57 @@ export const IdentityInfoSection: React.FC<IdentityInfoSectionProps> = (
 ) => {
   const { identityInfo } = props;
 
-  return (
-    <section>
-      <div className="flex items-start justify-between space-x-3">
-        <div className="flex items-center">
-          <Avatar
-            image={identityInfo.avatar}
-            alt={identityInfo.name}
-            size="large"
-            className="mr-3"
-          />
-          <div className="flex flex-col items-start">
-            <h2 className="text-xl font-bold">{identityInfo.name}</h2>
-            {identityInfo.veridaName && (
-              <span className="font-normal text-primary/60">
-                {identityInfo.veridaName}
-              </span>
-            )}
+  if (identityInfo) {
+    return (
+      <section>
+        <div className="flex items-start justify-between space-x-3">
+          <div className="flex items-center">
+            <Avatar
+              image={identityInfo.avatar}
+              alt={identityInfo.name}
+              size="large"
+              className="mr-3"
+            />
+            <div className="flex flex-col items-start">
+              <h2 className="text-xl font-bold">{identityInfo.name}</h2>
+              {identityInfo.veridaName && (
+                <span className="font-normal text-primary/60">
+                  {identityInfo.veridaName}
+                </span>
+              )}
+            </div>
+          </div>
+          <div className="flex items-center justify-between space-x-3">
+            <IconButton icon={<Icon type="share" />} />
+            <IconButton icon={<Icon type="more" />} />
           </div>
         </div>
-        <div className="flex items-center justify-between space-x-3">
-          <IconButton icon={<Icon type="share" />} />
-          <IconButton icon={<Icon type="more" />} />
-        </div>
+        {identityInfo.description && (
+          <p className="mt-4">{identityInfo.description}</p>
+        )}
+      </section>
+    );
+  }
+
+  return <IdentityInfoSectionSkeleton />;
+};
+
+function IdentityInfoSectionSkeleton() {
+  return (
+    <section className="animate-pulse">
+      <SkeletonBase className="mr-3 aspect-square h-16 opacity-10 sm:h-40" />
+      <div className="mt-4 flex flex-col space-y-2">
+        <SkeletonBase className="h-4 w-1/4 opacity-10" />
+        <SkeletonBase className="h-4 w-1/3 opacity-10" />
       </div>
-      {identityInfo.description && (
-        <p className="mt-4">{identityInfo.description}</p>
-      )}
+      <div className="mt-4 flex flex-col space-y-2">
+        <SkeletonBase className="h-4 w-full opacity-10" />
+        <div className="flex space-x-1">
+          <SkeletonBase className="h-4 w-2/3 opacity-10" />
+          <SkeletonBase className="h-4 flex-grow opacity-10" />
+        </div>
+        <SkeletonBase className="h-4 w-1/3 opacity-10" />
+      </div>
     </section>
   );
-};
+}
