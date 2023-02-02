c894668ca8aab973e66e829b0f45f2c907ee3036
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2023-02-02 22:16:13 +0100

    fix(styles): rework modal styles

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useId } from "react";
+import React, { useCallback } from "react";
 import { Icon, IconButton } from "components/atoms";
 import { PortalWrapper } from "./PortalWrapper";
 import { useEffect } from "react";
@@ -20,7 +20,6 @@ export const Modal: React.FunctionComponent<ModalProps> = ({
   handleClose,
   ...otherProps
 }) => {
-  const modalId = useId();
   const handleCloseOnEscapeKey = useCallback(
     (event: KeyboardEvent) => {
       if (event.key === KEY_NAME_ESC) {
@@ -43,26 +42,29 @@ export const Modal: React.FunctionComponent<ModalProps> = ({
   return (
     <PortalWrapper>
       <div className="fixed inset-0 z-50 flex min-h-full w-full bg-background/80 backdrop-blur-[10px]" />
-      <div className="fixed bottom-0 z-50 mx-auto w-full md:inset-0 md:flex md:max-w-2xl md:items-center md:justify-center">
-        <div
-          role="dialog"
-          id={modalId}
-          aria-modal="true"
-          aria-labelledby={title}
-          className="rounded-tl-3xl rounded-tr-3xl border border-solid border-gray-dark bg-background md:rounded-3xl"
-        >
-          <div className="flex items-center justify-between px-4 pt-6 md:px-8 md:pt-8">
-            <div />
-            <span className="text-lg font-semibold leading-5">{title}</span>
-            <IconButton
-              size="small"
-              variant="text"
-              onClick={handleClose}
-              icon={<Icon type="close" size={14} />}
-            />
-          </div>
-          <div className="flex flex-col justify-center p-4 md:p-8">
-            {children}
+      <div
+        className="relative"
+        aria-labelledby={title}
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className="fixed inset-0 z-50 overflow-y-auto">
+          <div className="z-50 flex min-h-full w-full items-end justify-center text-center sm:items-center sm:p-0">
+            <div className="relative w-full  overflow-hidden rounded-lg rounded-tl-3xl rounded-tr-3xl border border-solid border-gray-dark bg-background text-left shadow-xl transition-all sm:my-8 sm:max-w-lg md:rounded-3xl">
+              <div className="flex items-center justify-between px-4 pt-6 md:px-8 md:pt-8">
+                <div />
+                <span className="text-lg font-semibold leading-5">{title}</span>
+                <IconButton
+                  size="small"
+                  variant="text"
+                  onClick={handleClose}
+                  icon={<Icon type="close" size={14} />}
+                />
+              </div>
+              <div className="flex flex-col justify-center p-4 md:p-8">
+                {children}
+              </div>
+            </div>
           </div>
         </div>
       </div>
