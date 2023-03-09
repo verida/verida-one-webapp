2217480c9702df7416b2e6f09fc4e3b488a2bac7
aurelticot <aurelticot.dev@gmail.com>
2023-03-09 15:05:50 +1100

    remove badge type

--- src/lib/types/profile.ts
+++ src/lib/types/profile.ts
@@ -7,7 +7,6 @@ import {
   VerificationProofSchema,
   WalletAddressSchema,
 } from "lib/schemas";
-import { Chains } from "./chains";
 
 export type VerificationProof = z.infer<typeof VerificationProofSchema>;
 
@@ -20,23 +19,3 @@ export type WalletAddress = z.infer<typeof WalletAddressSchema>;
 export type FeaturedAsset = z.infer<typeof FeaturedAssetSchema>;
 
 export type ProfileData = z.infer<typeof ProfileDataSchema>;
-
-// ================
-
-// TODO: Update Badge types following what is returned by the API, when available.
-
-export interface Badge {
-  chain_id: Chains;
-  owner_address: string;
-  token_address: string;
-  token_id: string;
-  name: string | null;
-  metadata: {
-    name: string | null;
-    description: string | null;
-    image: string;
-    proofLabel: string; // Not sure, but assumed it would be something like "Twitter handle"/"Verida DID"/... in a user-friendly format
-    proofValue: string; // Not sure, but assumed it would be the actual value of the proof
-  };
-  isSBT: boolean;
-}
