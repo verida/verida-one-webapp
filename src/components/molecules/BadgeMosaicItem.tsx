415e2019880f0c80cfc7ea1e1c6d3c2d6e8ae30d
aurelticot <aurelticot.dev@gmail.com>
2023-03-26 12:56:08 +1100

    add fallback to missing or broken image in badge mosaic item

--- src/components/molecules/BadgeMosaicItem.tsx
+++ src/components/molecules/BadgeMosaicItem.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { HexagonBase, SkeletonBase } from "components/atoms";
+import React, { useCallback, useState } from "react";
+import { HexagonBase, Icon, SkeletonBase } from "components/atoms";
 import { Badge } from "lib/types";
 import { Link } from "react-router-dom";
+import { useIntl } from "react-intl";
 
 type HexagonProps = {
   badge: Badge;
@@ -13,6 +14,31 @@ export const BadgeMosaicItem: React.FunctionComponent<HexagonProps> = (
   props
 ) => {
   const { badge, to, disableShadow = false, ...otherProps } = props;
+  const source = badge.metadata.image;
+  const i18n = useIntl();
+  const [failedImage, setFailedImage] = useState(false);
+
+  const noContentMessage = i18n.formatMessage({
+    id: "BadgeMosaicItem.noContentMessage",
+    defaultMessage: "Content not available",
+    description:
+      "Message indicating that the asset has either no media content or this content is broken",
+  });
+
+  const imageClasses = "h-full w-full object-cover";
+
+  const handleImageLoad = useCallback(
+    (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+      setFailedImage(false);
+      // Set the final classes once we know the image is loaded
+      event.currentTarget.className = imageClasses;
+    },
+    []
+  );
+
+  const handleImageError = useCallback(() => {
+    setFailedImage(true);
+  }, []);
 
   return (
     <li
@@ -21,11 +47,25 @@ export const BadgeMosaicItem: React.FunctionComponent<HexagonProps> = (
     >
       <HexagonBase className="flex items-center justify-center">
         <Link to={to} className="h-full w-full">
-          <img
-            src={badge.metadata.image || undefined}
-            alt={badge.metadata.name || "Collectible"}
-            className="h-full w-full object-cover"
-          />
+          {source && !failedImage ? (
+            <img
+              src={source}
+              alt={badge.metadata.name || "Collectible"}
+              // Set temporary classes until it is overriden by the onLoad event
+              // bg-primary-15 is for the background color
+              // animate-pulse is for applying a loading animation
+              className={`animate-pulse bg-primary-15 ${imageClasses}`}
+              onLoad={handleImageLoad}
+              onError={handleImageError}
+            />
+          ) : (
+            <div className="flex h-full w-full items-center justify-center bg-primary-15 p-8">
+              <div className="flex flex-col items-center justify-center text-gray-light">
+                <Icon type="error-picture" size={48} />
+                <p className="text-center">{noContentMessage}</p>
+              </div>
+            </div>
+          )}
         </Link>
       </HexagonBase>
       {!disableShadow && (
