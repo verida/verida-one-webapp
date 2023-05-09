8585a5aed59ebdc12ef106a2d65b85c993192947
aurelticot <aurelticot.dev@gmail.com>
2023-05-09 17:12:34 +0200

    Use Verida IPFS gateway

--- src/lib/constants/ipfs.ts
+++ src/lib/constants/ipfs.ts
@@ -2,4 +2,4 @@
 export const IPFS_PROTOCOL = "ipfs://";
 
 /** An IPFS gateway URL. Does not have a trailing `/`. */
-export const IPFS_GATEWAY_URL = "https://dweb.link/ipfs";
+export const IPFS_GATEWAY_URL = "https://data.verida.network/ipfs";
